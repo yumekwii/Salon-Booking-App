ef666f94f9cb33c975984619bdb9a00223f004be
Reynold <yumekwii15@gmail.com>
2026-08-16 01:41:18 +0800

    updating the UI v15: UI (i hope this is the End)

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -63,7 +63,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4a0dc5b78e177d9e18762532d1fe4071
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "681395b3fa79e05f1b785a7e19f70fb4",
 "/": "681395b3fa79e05f1b785a7e19f70fb4",
-"main.dart.js": "dc287e0c8e3360b55bc11e02d675186b",
+"main.dart.js": "0be0260554b20a45582f8d6929458b71",
 "manifest.json": "f922346575463577f64caba5756a6835",
 "version.json": "23ba75600800dfd2f8d3488d16fded97"};
 // The application shell files that are downloaded before a service worker can
