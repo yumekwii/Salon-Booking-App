8a66d01cc81acc0293b26bbb693bd54329527d84
Reynold <yumekwii15@gmail.com>
2026-08-16 01:06:59 +0800

    updating the UI v13: UI

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -39,7 +39,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4a0dc5b78e177d9e18762532d1fe4071
 "assets/assets/images/treatments/scalp-detox.jpg": "b9853e9c8f98edb3dc5e1b38f939774a",
 "assets/assets/images/treatments/smart-bond.jpg": "cc8200337bd6aca123e145b2f18d4d13",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
-"assets/fonts/MaterialIcons-Regular.otf": "b59880447d3140b86f65aabe4c47c09a",
+"assets/fonts/MaterialIcons-Regular.otf": "dc7bf9437b0db898f1e3fc17a4209655",
 "assets/NOTICES": "182bda044c7c62882b57208480021f7e",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "canvaskit/canvaskit.js": "140ccb7d34d0a55065fbd422b843add6",
@@ -63,7 +63,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4a0dc5b78e177d9e18762532d1fe4071
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "681395b3fa79e05f1b785a7e19f70fb4",
 "/": "681395b3fa79e05f1b785a7e19f70fb4",
-"main.dart.js": "33b90fbc52e604d8312bb185d02f962a",
+"main.dart.js": "43575b4fcb0e382d7c2ba246dc50a3d2",
 "manifest.json": "f922346575463577f64caba5756a6835",
 "version.json": "23ba75600800dfd2f8d3488d16fded97"};
 // The application shell files that are downloaded before a service worker can
